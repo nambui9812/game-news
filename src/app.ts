3dda7ba5a299a6e60886aa95628b51bf3ea40c7a
nambui9812 <buihainam9812@gmail.com>
2020-12-31 14:20:55 -0700

    update

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import helmet from 'helmet';
 import { createConnection } from 'typeorm';
 
 import 'reflect-metadata';
@@ -14,6 +15,7 @@ const initServer = () => {
     const app = express();
     
     // Use middlewares
+    app.use(helmet());
     app.use(cors());
     app.use(morgan('dev'));
     app.use(express.json());
